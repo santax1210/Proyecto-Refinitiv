2d99229af61103df8faefcaf4e68ee8b07ad8dac
Santax <santiago1210salvador@gmail.com>
2026-03-17 09:41:27 -0300

    limpieza scripts-archivos

--- tmp_get_industries.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'c:\Users\Santiago\Documents\allocations_validation\data\raw\sector\allocations_nuevas.csv', sep=';', encoding='latin-1', dtype=str)
-
-if 'classif' in df.columns:
-    df_industry = df[df['classif'].str.strip().str.lower() == 'industry']
-    print(df_industry['class'].unique())
-else:
-    print('No classif column found')
